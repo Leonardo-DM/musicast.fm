b6b7bd5dc0d829bc843462a139c86b9f49d7b7b7
Léo_Dev <69600417+Leonardo-DM@users.noreply.github.com>
2022-08-08 23:51:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,17 +9,17 @@
 */
 
 var settings = {
-    'radio_name': 'Brasil Hits',
+    'radio_name': 'Deboxe Funk Brasil Hits',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
-    'url_streaming': 'https://streaming.brasilhits.com/acc',
+    'url_streaming': 'http://stream.zeno.fm/rzywd1sz8zhvv',
 
     // When choosing icecast, make sure the file 'player.log' in the document root is writeable.
     // Choose between 'shoutcast' or 'icecast'
     'streaming_type': 'icecast',
 
     // Visit https://api.vagalume.com.br/docs/ to get your API key
-    'api_key': '18fe07917957c289983464588aabddfb',
+    'api_key': 'f99c41384eeb8a2ef898f5a5e997c28c',
 
     // Set to true to get the last songs played
     'historic': true,
@@ -30,5 +30,5 @@ var settings = {
     'next_song': false,
 
     // Path to default cover art when none are found
-    'default_cover_art': 'img/bg-capa.jpg',
+    'default_cover_art': 'img/bg-l30.jpg',
 };
